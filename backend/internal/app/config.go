e64404f9c06b43bb38ee4717e60e098f201b6b0e
Chepigo Darya <chepigods@student.bmstu.ru>
2023-11-01 18:51:01 +0300

    lab_05 | add mirror and cache

--- backend/internal/app/config.go
+++ backend/internal/app/config.go
@@ -1,8 +1,8 @@
 package app
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
+	// "fmt"
+	// "github.com/joho/godotenv"
 	"os"
 )
 
@@ -23,21 +23,21 @@ type Config struct {
 }
 
 func (c *Config) ParseConfig() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("error!!!")
-		// log.Fatalf("Some error occured. Err: %s", err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	fmt.Println("error!!!")
+	// 	// log.Fatalf("Some error occured. Err: %s", err)
+	// }
 
 	c.Address = os.Getenv("BACKEND_HOST")
 	c.Port = os.Getenv("BACKEND_PORT")
 	c.LogLevel = os.Getenv("LOG_LEVEL")
 	c.LogFile = os.Getenv("LOG_FILE")
 
-	c.Postgres.Host = os.Getenv("POSTGRES_HOST")
-	c.Postgres.Port = os.Getenv("POSTGRES_PORT")
-	c.Postgres.User = os.Getenv("POSTGRES_USER")
-	c.Postgres.Password = os.Getenv("POSTGRES_PASSWORD")
-	c.Postgres.DBName = os.Getenv("POSTGRES_DB")
+	c.Postgres.Host = os.Getenv("POSTGRESQL_HOST")
+	c.Postgres.Port = os.Getenv("POSTGRESQL_PORT")
+	c.Postgres.User = os.Getenv("POSTGRESQL_USERNAME")
+	c.Postgres.Password = os.Getenv("POSTGRESQL_PASSWORD")
+	c.Postgres.DBName = os.Getenv("POSTGRESQL_DB")
 
 }
